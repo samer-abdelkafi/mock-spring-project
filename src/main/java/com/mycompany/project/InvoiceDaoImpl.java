1456ddf85b2eaf2becac233b048e72224501c339
sam <samer.abdelkafi@gmail.com>
2015-09-16 17:18:53 +0200

    first commit

--- src/main/java/com/mycompany/project/InvoiceDaoImpl.java
+++ src/main/java/com/mycompany/project/InvoiceDaoImpl.java
@@ -3,11 +3,7 @@ package com.mycompany.project;
 import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
-import java.util.Random;
 
-/**
- * Created by b002ihq on 16/09/2015.
- */
 @Component
 public class InvoiceDaoImpl implements InvoiceDao {
 
--- src/main/java/com/mycompany/project/InvoiceService.java
+++ src/main/java/com/mycompany/project/InvoiceService.java
@@ -3,9 +3,6 @@ package com.mycompany.project;
 import java.sql.SQLException;
 import java.util.Map;
 
-/**
- * Created by b002ihq on 16/09/2015.
- */
 public interface InvoiceService {
 
      Invoice processInvoice(Map<Product, Integer> products) throws SQLException;
